e56348f28e98760d3caa59200a5b107859f736f1
Lei Chen <lei8c8@gmail.com>
2019-12-18 21:30:22 -0500

    Create intersection_of_two_linked_list.py

---
+++ linked_list/intersection_of_two_linked_list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        curA, curB = headA, headB
+        cA, cB = 0, 0
+        while True:
+            if curA == curB:
+                return curA
+            if curA.next:
+                curA = curA.next 
+            else:
+                curA = headB 
+                cA += 1
+            if curB.next:
+                curB = curB.next 
+            else:
+                curB = headA
+                cB += 1
+            if cA > 1 or cB > 1:
+                return None
