f0795bf33ee95da39ea746b13eb56b633a1b874b
Lei Chen <lei8c8@gmail.com>
2019-12-27 18:38:35 -0500

    Update group_shifted_strings.py

--- hash_table/group_shifted_strings.py
+++ hash_table/group_shifted_strings.py
@@ -4,7 +4,7 @@ class Solution:
         grouped_string, res = defaultdict(list), []
         for string in strings:
             key = [ord(s) - ord(string[0]) for s in string]
-            key = tuple([number + 26 for number in key if number < 0])
+            key = tuple([number + 26 if number < 0 else number for number in key])
             grouped_string[key].append(string)
         for key in grouped_string:
             res.append(grouped_string[key])
