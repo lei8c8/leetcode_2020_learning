e1da180bb38eb639ceacc0c066d7142b17fc28cf
Lei Chen <lei8c8@gmail.com>
2019-12-27 18:32:06 -0500

    Create group_shifted_strings.py

---
+++ hash_table/group_shifted_strings.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        grouped_string, res = defaultdict(list), []
+        for string in strings:
+            key = [ord(s) - ord(string[0]) for s in string]
+            key = tuple([number + 26 for number in key if number < 0])
+            grouped_string[key].append(string)
+        for key in grouped_string:
+            res.append(grouped_string[key])
+        return res 
