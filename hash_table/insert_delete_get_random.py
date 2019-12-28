e4484f99e66771c84d2cdba5af2be4593d0182b8
Lei Chen <lei8c8@gmail.com>
2019-12-27 19:55:04 -0500

    Create insert_delete_get_random.py

---
+++ hash_table/insert_delete_get_random.py
@@ -0,0 +1,37 @@
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.items = set()
+        
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.items:
+            return False
+        self.items.add(val)
+        return True 
+        
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val not in self.items:
+            return False
+        self.items.remove(val)
+        return True
+        
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        item = random.choice(list(self.items))
+        return item
