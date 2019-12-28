9410cf2b862f8a657b6fad5b46f4953e7ae43dab
Lei Chen <lei8c8@gmail.com>
2019-12-27 19:47:27 -0500

    Create unique_word.py

---
+++ hash_table/unique_word.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class ValidWordAbbr:
+
+    def __init__(self, dictionary):
+        self.seen = collections.defaultdict(set)
+        for word in dictionary:
+            self.seen[self.to_abbr(word)].add(word)
+       
+    def to_abbr(self, word):
+        if len(word) <= 2: return word  
+        return word[0] + str(len(word[1:-1])) + word[-1]
+
+    def isUnique(self, word):
+        abbr = self.to_abbr(word)
+        if abbr not in self.seen:
+            return True
+        return word in self.seen[abbr] and len(self.seen[abbr]) == 1
